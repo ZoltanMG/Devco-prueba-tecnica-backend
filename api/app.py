3dbbda69634275ae3abe2a4951aad62270fb45d9
Zoltan MG <zoltanmoragarcia@gmail.com>
2021-08-20 11:47:00 -0500

    [add]: se agregó flask_cors para habilitar Access-Control-Allow-Origin en http://localhost:3000

--- api/app.py
+++ api/app.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def home():
     return {"estado": "OK"}
+
+
+@app.after_request
+def after(response):
+    # This function verifies the session after a request
+    # and gives the CORS with the frontend
+    # allows the conncetion between the back and the front
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return(response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
